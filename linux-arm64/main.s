352e0f3114808961d593d37a66c208a037d9f2d6
Bressan <g.br3ssan@gmail.com>
2023-12-21 21:54:30 +0000

    * Removed unnecessary logic

--- linux-arm64/main.s
+++ linux-arm64/main.s
@@ -8,10 +8,22 @@ strlen: .word 21
 .global _start
 
 exit:
-	mov x8, #93
-	mov x0, #0x0
-	mov x16, #0x0
-	svc #0x0
+	mov x8, #93			// linux exit syscall
+	// mov x0, #0x0 // status argument
+	svc #0x0				// supervisor call (throw exception to EL1)		
+	ret
+
+print_msg:
+	mov x8,	#64				// write syscall
+	mov x0,	#0x0			// STDOUT_FD
+	ldr x1,	=str			// buffer
+	mov w2, [strlen]	// buflen
+	svc #0x0					// supervisor call
+	ret								// branch to the address at x30
 
 _start:
+	stp x29, x30, [sp, #-16]!
+	bl print_msg
+	mov x0, #0x0
 	b exit
+	ldp x29, x30, [sp], #16
